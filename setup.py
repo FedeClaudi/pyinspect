b1bc144cee2a552ec462a7d077a23bca80be57d0
FedeClaudi <federicoclaudi@protonmail.com>
2020-09-30 11:10:07 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-requirements = ["mdutils", "astunparse", "treelib"]
+requirements = [
+    "rich",
+]
 
 setup(
-    name="pydoc2md",
-    version="0.0.0.2",
-    description="Converts python code to markdown docs",
+    name="pyinspect",
+    version="0.0.1",
+    description="Find and inspect python functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=requirements,
@@ -18,8 +20,8 @@ setup(
     python_requires=">=3.6,",
     packages=find_namespace_packages(),
     include_package_data=True,
-    url="https://github.com/FedeClaudi/pydoc2md",
+    url="https://github.com/FedeClaudi/pyinspect",
     author="Federico Claudi",
     zip_safe=False,
-    entry_points={"console_scripts": ["pydoc2md = pydoc2md.cli:run"]},
+    # entry_points={"console_scripts": ["pydoc2md = pydoc2md.cli:run"]},
 )
